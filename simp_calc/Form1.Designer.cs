23d871a72348f6a2ab2433733618ad5c85f766ec
zivile sakiene <z.sakiene@gmail.com>
2017-04-21 10:55:26 +0300

    sukurtas projektas

---
+++ simp_calc/Form1.Designer.cs
@@ -0,0 +1,246 @@
+﻿namespace simp_calc
+{
+    partial class Skaiciuokle
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.num1 = new System.Windows.Forms.TextBox();
+            this.num2 = new System.Windows.Forms.TextBox();
+            this.sudeti = new System.Windows.Forms.Button();
+            this.atimti = new System.Windows.Forms.Button();
+            this.dalinti = new System.Windows.Forms.Button();
+            this.dauginti = new System.Windows.Forms.Button();
+            this.kel_l = new System.Windows.Forms.Button();
+            this.faktorialas = new System.Windows.Forms.Button();
+            this.num_fact = new System.Windows.Forms.TextBox();
+            this.ats = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button6 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // num1
+            // 
+            this.num1.Location = new System.Drawing.Point(187, 38);
+            this.num1.Name = "num1";
+            this.num1.Size = new System.Drawing.Size(100, 20);
+            this.num1.TabIndex = 0;
+            // 
+            // num2
+            // 
+            this.num2.Location = new System.Drawing.Point(187, 83);
+            this.num2.Name = "num2";
+            this.num2.Size = new System.Drawing.Size(100, 20);
+            this.num2.TabIndex = 1;
+            // 
+            // sudeti
+            // 
+            this.sudeti.BackColor = System.Drawing.Color.MintCream;
+            this.sudeti.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.sudeti.Location = new System.Drawing.Point(131, 121);
+            this.sudeti.Name = "sudeti";
+            this.sudeti.Size = new System.Drawing.Size(41, 31);
+            this.sudeti.TabIndex = 2;
+            this.sudeti.Text = "+";
+            this.sudeti.UseVisualStyleBackColor = false;
+            this.sudeti.Click += new System.EventHandler(this.sudeti_Click_1);
+            // 
+            // atimti
+            // 
+            this.atimti.BackColor = System.Drawing.Color.MintCream;
+            this.atimti.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.atimti.Location = new System.Drawing.Point(178, 121);
+            this.atimti.Name = "atimti";
+            this.atimti.Size = new System.Drawing.Size(42, 31);
+            this.atimti.TabIndex = 3;
+            this.atimti.Text = "-";
+            this.atimti.UseVisualStyleBackColor = false;
+            this.atimti.Click += new System.EventHandler(this.atimti_Click);
+            // 
+            // dalinti
+            // 
+            this.dalinti.BackColor = System.Drawing.Color.MintCream;
+            this.dalinti.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dalinti.Location = new System.Drawing.Point(272, 121);
+            this.dalinti.Name = "dalinti";
+            this.dalinti.Size = new System.Drawing.Size(40, 31);
+            this.dalinti.TabIndex = 5;
+            this.dalinti.Text = "/";
+            this.dalinti.UseVisualStyleBackColor = false;
+            this.dalinti.Click += new System.EventHandler(this.dalinti_Click);
+            // 
+            // dauginti
+            // 
+            this.dauginti.BackColor = System.Drawing.Color.MintCream;
+            this.dauginti.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dauginti.Location = new System.Drawing.Point(226, 121);
+            this.dauginti.Name = "dauginti";
+            this.dauginti.Size = new System.Drawing.Size(40, 31);
+            this.dauginti.TabIndex = 4;
+            this.dauginti.Text = "*";
+            this.dauginti.UseVisualStyleBackColor = false;
+            this.dauginti.Click += new System.EventHandler(this.dauginti_Click);
+            // 
+            // kel_l
+            // 
+            this.kel_l.BackColor = System.Drawing.Color.MintCream;
+            this.kel_l.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.kel_l.Location = new System.Drawing.Point(318, 121);
+            this.kel_l.Name = "kel_l";
+            this.kel_l.Size = new System.Drawing.Size(44, 31);
+            this.kel_l.TabIndex = 7;
+            this.kel_l.Text = "x^y";
+            this.kel_l.UseVisualStyleBackColor = false;
+            this.kel_l.Click += new System.EventHandler(this.kel_l_Click);
+            // 
+            // faktorialas
+            // 
+            this.faktorialas.BackColor = System.Drawing.Color.MintCream;
+            this.faktorialas.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.faktorialas.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.faktorialas.Location = new System.Drawing.Point(132, 189);
+            this.faktorialas.Name = "faktorialas";
+            this.faktorialas.Size = new System.Drawing.Size(37, 33);
+            this.faktorialas.TabIndex = 6;
+            this.faktorialas.Text = "!";
+            this.faktorialas.UseVisualStyleBackColor = false;
+            this.faktorialas.Click += new System.EventHandler(this.faktorialas_Click);
+            // 
+            // num_fact
+            // 
+            this.num_fact.Location = new System.Drawing.Point(26, 197);
+            this.num_fact.Name = "num_fact";
+            this.num_fact.Size = new System.Drawing.Size(100, 20);
+            this.num_fact.TabIndex = 8;
+            // 
+            // ats
+            // 
+            this.ats.Cursor = System.Windows.Forms.Cursors.No;
+            this.ats.Enabled = false;
+            this.ats.Location = new System.Drawing.Point(344, 239);
+            this.ats.Name = "ats";
+            this.ats.Size = new System.Drawing.Size(131, 20);
+            this.ats.TabIndex = 9;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(277, 242);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(58, 13);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "Atsakymas";
+            // 
+            // button6
+            // 
+            this.button6.BackColor = System.Drawing.Color.MintCream;
+            this.button6.Location = new System.Drawing.Point(344, 210);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(75, 23);
+            this.button6.TabIndex = 11;
+            this.button6.Text = "Skaičiuoti =";
+            this.button6.UseVisualStyleBackColor = false;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(23, 173);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(116, 13);
+            this.label2.TabIndex = 12;
+            this.label2.Text = "Faktorialo skaičiavimas";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(199, 22);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(79, 13);
+            this.label4.TabIndex = 14;
+            this.label4.Text = "Pirmas skaičius";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(199, 67);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(78, 13);
+            this.label5.TabIndex = 15;
+            this.label5.Text = "Antras skaičius";
+            // 
+            // Skaiciuokle
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightGreen;
+            this.ClientSize = new System.Drawing.Size(504, 275);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.ats);
+            this.Controls.Add(this.num_fact);
+            this.Controls.Add(this.kel_l);
+            this.Controls.Add(this.faktorialas);
+            this.Controls.Add(this.dalinti);
+            this.Controls.Add(this.dauginti);
+            this.Controls.Add(this.atimti);
+            this.Controls.Add(this.sudeti);
+            this.Controls.Add(this.num2);
+            this.Controls.Add(this.num1);
+            this.Name = "Skaiciuokle";
+            this.Text = "Calculator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        
+        private System.Windows.Forms.Button sudeti;
+        private System.Windows.Forms.Button atimti;
+        private System.Windows.Forms.Button dalinti;
+        private System.Windows.Forms.Button dauginti;
+        private System.Windows.Forms.Button kel_l;
+        private System.Windows.Forms.Button faktorialas;
+     
+        private System.Windows.Forms.TextBox num1;
+        private System.Windows.Forms.TextBox num2;
+        private System.Windows.Forms.TextBox num_fact;
+        private System.Windows.Forms.TextBox ats;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ simp_calc/Form1.cs
@@ -0,0 +1,237 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Text.RegularExpressions;
+
+namespace simp_calc
+{
+    public partial class Skaiciuokle : Form
+    {
+        private double atsa = 0;
+
+        public Skaiciuokle()
+        {
+            InitializeComponent();
+        }
+
+
+        private void sudeti_Click_1(object sender, EventArgs e)
+        {
+
+            bool kintXteisingas = Regex.IsMatch(num1.Text, @"^-?\d+(\.\d*)?$");
+            bool kintYteisingas = Regex.IsMatch(num2.Text, @"^-?\d+(\.\d*)?$");
+
+            if (!kintXteisingas)
+            {
+                MessageBox.Show("1 skaičius neteisingas arba tuščias");
+                return;
+            }
+            if (!kintYteisingas)
+            {
+                MessageBox.Show("2 skaičius neteisingas arba tuščias");
+                return;
+            }
+            if (kintXteisingas == true || kintYteisingas == true)
+            {
+                double number1 = Convert.ToDouble(num1.Text);
+                double number2 = Convert.ToDouble(num2.Text);
+                atsa = PaprastosFunkcijos.sudetis(number1, number2);
+            }
+        }
+
+
+
+        private void atimti_Click(object sender, EventArgs e)
+        {
+
+            bool kintXteisingas = Regex.IsMatch(num1.Text, @"^-?\d+(\.\d*)?$");
+            bool kintYteisingas = Regex.IsMatch(num2.Text, @"^-?\d+(\.\d*)?$");
+
+            if (!kintXteisingas)
+            {
+                MessageBox.Show("neteisingas pirmas sk");
+                return;
+            }
+            if (!kintYteisingas)
+            {
+                MessageBox.Show("2 skaičius neteisingas arba tuščias");
+                return;
+            }
+            if (kintXteisingas == true || kintYteisingas == true)
+            {
+                double number1 = Convert.ToDouble(num1.Text);
+                double number2 = Convert.ToDouble(num2.Text);
+
+                atsa = PaprastosFunkcijos.atimtis(number1, number2);
+            }
+        }
+
+        private void dauginti_Click(object sender, EventArgs e)
+        {
+
+            bool kintXteisingas = Regex.IsMatch(num1.Text, @"^-?\d+(\.\d*)?$");
+            bool kintYteisingas = Regex.IsMatch(num2.Text, @"^-?\d+(\.\d*)?$");
+
+            if (!kintXteisingas)
+            {
+                MessageBox.Show("1 skaičius neteisingas arba tuščias");
+                return;
+            }
+            if (!kintYteisingas)
+            {
+                MessageBox.Show("2 skaičius neteisingas arba tuščias");
+                return;
+            }
+            if (kintXteisingas == true || kintYteisingas == true)
+            {
+
+                double number1 = Convert.ToDouble(num1.Text);
+                double number2 = Convert.ToDouble(num2.Text);
+
+                atsa = PaprastosFunkcijos.daugyba(number1, number2);
+            }
+        }
+
+        private void dalinti_Click(object sender, EventArgs e)
+        {
+
+            bool kintXteisingas = Regex.IsMatch(num1.Text, @"^-?\d+(\.\d*)?$");
+            bool kintYteisingas = Regex.IsMatch(num2.Text, @"^-?\d+(\.\d*)?$");
+
+            if (!kintXteisingas)
+            {
+                MessageBox.Show("1 skaičius neteisingas arba tuščias");
+                return;
+            }
+            if (!kintYteisingas)
+            {
+                MessageBox.Show("2 skaičius neteisingas arba tuščias");
+                return;
+            }
+            if (kintXteisingas == true || kintYteisingas == true)
+            {
+                double number1 = Convert.ToDouble(num1.Text);
+                double number2 = Convert.ToDouble(num2.Text);
+
+                atsa = PaprastosFunkcijos.dalyba(number1, number2);
+            }
+        }
+
+        private void kel_l_Click(object sender, EventArgs e)
+        {
+            bool kintXteisingas = Regex.IsMatch(num1.Text, @"^-?\d+(\.\d*)?$");
+            bool kintYteisingas = Regex.IsMatch(num2.Text, @"^-?\d+(\.\d*)?$");
+
+            if (!kintXteisingas)
+            {
+                MessageBox.Show("1 skaičius neteisingas arba tuščias");
+                return;
+            }
+            if (!kintYteisingas)
+            {
+                MessageBox.Show("2 skaičius neteisingas arba tuščias");
+                return;
+            }
+            if (kintXteisingas == true || kintYteisingas == true)
+            {
+                double number1 = Convert.ToDouble(num1.Text);
+                double number2 = Convert.ToDouble(num2.Text);
+                atsa = SudetingosFunkcijos.kelimasLaipsniu(number1, number2);
+            }
+
+
+        }
+
+        private void faktorialas_Click(object sender, EventArgs e)
+        {
+
+            bool fak_number = Regex.IsMatch(num_fact.Text, @"^\d+$");
+            if (fak_number == false)
+            {
+                MessageBox.Show("Įveskite teigiamą skaičių");
+                return;
+            }
+            else
+            {
+                ulong fakt_num = Convert.ToUInt64(num_fact.Text);
+                atsa = SudetingosFunkcijos.faktorialas(fakt_num);
+            }
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            ats.Text = atsa.ToString();
+        }
+
+
+
+
+    }
+}
+
+
+
+
+class PaprastosFunkcijos
+{
+    public static double sudetis(double a, double b)
+    {
+        return a + b;
+    }
+
+    public static double atimtis(double a, double b)
+    {
+        return a - b;
+    }
+
+    public static double daugyba(double a, double b)
+    {
+        return a * b;
+    }
+
+    public static ulong daugyba(ulong a, ulong b)
+    {
+        return a * b;
+    }
+
+    public static double dalyba(double a, double b)
+    {
+        return a / b;
+    }
+}
+
+class SudetingosFunkcijos
+{
+
+    public static ulong faktorialas(ulong a)
+    {
+        ulong i = 1;
+        ulong fakt = 1;
+        while (i <= a)
+        {
+            fakt = PaprastosFunkcijos.daugyba(fakt, i);
+            i++;
+        }
+        return fakt;
+    }
+    public static double kelimasLaipsniu(double c, double d)
+    {
+
+        int i = 1;
+        double pakeltas_skaic = 1;
+        while (i <= d)
+        {
+            pakeltas_skaic = PaprastosFunkcijos.daugyba(pakeltas_skaic, c);
+            i++;
+        }
+        return pakeltas_skaic;
+    }
+
+}
+
